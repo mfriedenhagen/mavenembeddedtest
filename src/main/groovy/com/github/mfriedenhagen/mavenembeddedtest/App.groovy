fab81ab0aa4fe17d6fdf4db8845f6db051376ea0
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2015-06-17 21:25:59 +0200

    Add dependency for YAML configuration and add classworld to avoid  java.util.NoSuchElementException                                                                         role: org.apache.maven.eventspy.internal.EventSpyDispatcher (not working)

--- src/main/groovy/com/github/mfriedenhagen/mavenembeddedtest/App.groovy
+++ src/main/groovy/com/github/mfriedenhagen/mavenembeddedtest/App.groovy
@@ -1,6 +1,8 @@
 package com.github.mfriedenhagen.mavenembeddedtest
 
+import org.apache.maven.cli.CliRequest
 import org.apache.maven.cli.MavenCli
+import org.codehaus.plexus.classworlds.ClassWorld
 
 /**
  * Hello world!
@@ -8,10 +10,12 @@ import org.apache.maven.cli.MavenCli
  */
 public class App {
     private final cli = new MavenCli()
+    private final ClassWorld myClassWorld = new ClassWorld("plexus.core", getClass().getClassLoader());
 
     public App(String[] strings) {
         System.properties['maven.multiModuleProjectDirectory'] = ''
-        cli.doMain(strings, '.', System.out, System.err)
+        cli.doMain(new CliRequest(strings, myClassWorld))
+        //cli.doMain(strings, '.', System.out, System.err)
     }
 
     public static void main(String[] args) {
